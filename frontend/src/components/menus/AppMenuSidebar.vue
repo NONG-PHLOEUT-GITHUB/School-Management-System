f386f51132f4699f2cbd619acfa90998c4f92798
nong phloeut <phloeutnong@.com>
2023-10-30 18:08:11 +0700

    add

---
+++ frontend/src/components/menus/AppMenuSidebar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <h1>Main Navigation</h1>
+      <MenuList :menuItems="menuItems"></MenuList>
+    </div>
+  </template>
+  
+  <script>
+  import MenuList from './menuItems/MenuList.vue'; // Adjust the import path based on your project structure
+  
+  export default {
+    name: 'AppMenuSidebar',
+    components: {
+      MenuList,
+    },
+    data() {
+      return {
+        menuItems: [
+          {
+            title: 'Dashboard',
+            to: '/',
+          },
+          {
+            title: 'Charts',
+            children: [
+              {
+                title: 'Sub Link',
+                to: '/charts/sublink',
+              },
+            ],
+          },
+        ],
+      };
+    },
+  };
+  </script>
+  
